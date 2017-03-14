e3d0d9fc606bb18766602ffd413be82a21f99bb8
william chou <w.chou06@gmail.com>
2017-03-14 13:39:04 -0400

    Update and delete a record.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find(1)
+puts @store1.name
+@store2 = Store.find(2)
+@store1.name = "VanCity Baby"
+puts @store1.name
+puts @store2.name
 # Your code goes here ...
